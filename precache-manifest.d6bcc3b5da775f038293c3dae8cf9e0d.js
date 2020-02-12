37bd19fd4b3cae0d057bff4f63066d4b83267959
joanafpt <joanajteixeira@outlook.com>
2020-02-12 12:19:43 +0000

    body-mass-index-app with reset button included

---
+++ precache-manifest.d6bcc3b5da775f038293c3dae8cf9e0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e66135501fdcd61ec28108ea99ae835",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf0533e819af04a7e910",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ccd70463331868822f27",
+    "url": "./static/css/main.6879412d.chunk.css"
+  },
+  {
+    "revision": "cf0533e819af04a7e910",
+    "url": "./static/js/2.27dea20a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.27dea20a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccd70463331868822f27",
+    "url": "./static/js/main.18c26420.chunk.js"
+  },
+  {
+    "revision": "12ffc9a55ad7c713faeb",
+    "url": "./static/js/runtime-main.4a80a9d9.js"
+  }
+]);
